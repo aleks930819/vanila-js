a378102547c6b0edcda1160c97a3573024fff26b
Aleksandar Atanasov <aleks930819@gmail.com>
2023-09-29 20:57:20 +0300

    added dynamic routes

--- services/Menu.js
+++ services/Menu.js
@@ -2,14 +2,14 @@
 import API from "./API.js";
 
 export async function loadData() {
-  app.store.menu = await API.fetchMenu();
+  window.app.store.menu = await API.fetchMenu();
 }
 
 export async function getProductById(id) {
-  if (app.store.menu == null) {
+  if (window.app.store.menu == null) {
     await loadData();
   }
-  for (let c of app.store.menu) {
+  for (let c of window.app.store.menu) {
     for (let p of c.products) {
       if (p.id == id) {
         return p;
--- services/Router.js
+++ services/Router.js
@@ -3,8 +3,13 @@ const Router = {
     document.querySelectorAll("a").forEach((link) => {
       link.addEventListener("click", (e) => {
         e.preventDefault();
-        Router.go(e.target.href);
+        const route = e.target.href.replace(window.location.origin, "");
+        Router.go(route);
       });
+    });
+
+    window.addEventListener("popstate", (e) => {
+      Router.go(e.state.route, false);
     });
 
     Router.go(window.location.pathname, false);
@@ -12,6 +17,39 @@ const Router = {
   go: (route, addToHistory = true) => {
     if (addToHistory) {
       window.history.pushState({ route }, "", route);
+    }
+    let pageElement;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Home";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Order";
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          const id = route.replace("/product-", "");
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Product " + id;
+          pageElement.dataset.id = id;
+        }
+    }
+
+    if (pageElement == null) {
+      pageElement = document.createElement("h1");
+      pageElement.textContent = "404";
+    } else {
+      const cache = document.querySelector("main");
+      cache.childNodes.forEach((node) => {
+        node.remove();
+      });
+
+      cache.appendChild(pageElement);
+      window.screenX = 0;
+      window.screenY = 0;
     }
   },
 };
